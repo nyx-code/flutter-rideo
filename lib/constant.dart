05547ba9a00939f3252efcfab415649cae711f9c
Shailesh Kadam <kadamshailesh007@gmail.com>
2020-11-23 19:03:49 +0530

    Required constant files are added.

---
+++ lib/constant.dart
@@ -0,0 +1,32 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xFF32A663);
+const Color secondaryColor = Color(0xFFE8F6E8);
+const Color darkGreenColor = Color(0xFF0F532C);
+const Color white = Color(0xFFFFFFFF);
+const Color lightGrey = Color(0xFFF1F1F1);
+const Color darkGrey = Color(0xFF707070);
+const Color black = Color(0xFF242424);
+
+const double paddingS = 8.0;
+const double paddingM = 16.0;
+const double paddingL = 32.0;
+
+const double spaceS = 8.0;
+const double spaceM = 16.0;
+
+const double headline1 = 96.0;
+const double headline2 = 60.0;
+const double headline3 = 48.0;
+const double headline4 = 34.0;
+const double headline5 = 24.0;
+const double headline6 = 20.0;
+const double subtitle1 = 16.0;
+const double subtitle2 = 14.0;
+const double body1 = 16.0;
+const double body2 = 14.0;
+const double button = 14.0;
+const double caption = 12.0;
+const double overline = 10.0;
